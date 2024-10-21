10e380b96cd4e77a9080162c06e54695ecec33f3
koryos77 <k.rouget21@gmail.com>
2024-10-21 13:46:45 +0200

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h> /* prototype preprocessing*/
+
+int main(void) /*main function with not return (void) */
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
