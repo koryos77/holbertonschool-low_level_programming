50286dffff2084e98de7213901660711c73c66db
koryos77 <k.rouget21@gmail.com>
2024-10-22 16:04:54 +0200

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Print the last digit of the number stored in the variable n
+ * Return: Always 0 if Success.
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 15; i++)
+	{
+		char hex[] = "0123456789abcdef";
+		putchar(hex[i]);
+	}
+	putchar('\n');
+	return (0);
+}
