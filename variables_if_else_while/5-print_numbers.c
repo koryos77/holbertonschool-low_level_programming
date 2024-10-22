ecea1a81adddc1baf48f0261ba00878c00104966
koryos77 <k.rouget21@gmail.com>
2024-10-22 14:16:27 +0200

    printf

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Print the last digit of the number stored in the variable n
+ * Return: Always 0 if Success.
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+printf("%d", digit);
+}
+printf("\n");
+return (0);
+}
