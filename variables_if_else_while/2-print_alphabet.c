ff9020fba7c9cb966002f99ef32957934710ada9
koryos77 <k.rouget21@gmail.com>
2024-10-22 11:29:59 +0200

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Print the last digit of the number stored in the variable n
+ * Return: Always 0 if Success.
+ */
+int main(void)
+{
+char i = 'a';
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+return (0);
+}
