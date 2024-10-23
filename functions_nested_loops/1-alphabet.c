652c69c38bf6242758753c359d82c7c04005d7f2
koryos77 <k.rouget21@gmail.com>
2024-10-23 14:48:49 +0200

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - Entry Point
+ * function - print alphabet
  *
  * Description: Print alphabet with created functions
  * Return: Always 0 if Success.
  */
-void print_alphabet(void)
+void print_alphabet(void) 
 {
 	char i = 'a';
 
