553156e990afa452b541f6c73c20ba1b5d4358e3
koryos77 <k.rouget21@gmail.com>
2024-10-24 15:38:04 +0200

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - This function that checks for lowercase character.
+ * @r: checks for return
+ * Return: on Sucess 1
+ * On error, 0 is returned
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
