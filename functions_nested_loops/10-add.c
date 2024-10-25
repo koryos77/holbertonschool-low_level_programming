b37e1be431e7ddb891d798260e5fdd69ecb89000
koryos77 <k.rouget21@gmail.com>
2024-10-25 10:20:59 +0200

    add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * add - This function that add int
- *
+ * @a: new int
+ * @b: new int
  * Return: on Sucess 1
  * On error, 0 is returned
  */
 
-int add(int a, int b) {
-    return a + b; 
+int add(int a, int b)
+{
+	return (a + b);
 }
