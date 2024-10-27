f262101af449d60ac4f58a93abb43598ea1f0d57
koryos77 <k.rouget21@gmail.com>
2024-10-27 10:29:04 +0100

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,30 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * add - This function that add int
- * @a: new int
- * @b: new int
+ * print_to_98 - This function print all number to 98
+ * @n: new int
  * Return: on Sucess 1
  * On error, 0 is returned
  */
 
 void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+			printf("\n");
+	}
+}
