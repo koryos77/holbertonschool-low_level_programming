1d477b13aa34eccdb65163b6189ec7e5d272c91c
koryos77 <k.rouget21@gmail.com>
2024-10-27 12:18:04 +0100

    4000000fibonacci

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - is the main function
+ *
+ * Return: on success 0
+ * On error,1 is returned
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long first = 1;
+	unsigned long sec = 2;
+	unsigned long next = 0;
+	int limit = 98;
+
+	printf("%lu", first);
+
+	if (limit >= 2)
+	printf(", %lu", sec);
+
+	for (i = 3; i <= limit; i++)
+	{
+		next = first + sec;
+
+		if (next >= 4000000)
+		{
+			break;
+		}
+
+		printf(", %lu", next);
+		first = sec;
+		sec = next;
+	}
+
+	printf("\n");
+
+	return (0);
+}
