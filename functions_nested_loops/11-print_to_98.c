51d6d934d3e59fadd9901b40de7bc32055eeb21a
koryos77 <k.rouget21@gmail.com>
2024-10-27 10:36:27 +0100

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,18 +13,18 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
-		printf("\n");
+		printf("98\n");
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
 			printf("%d, ", i);
 		}
-			printf("\n");
+			printf("98\n");
 	}
 }
