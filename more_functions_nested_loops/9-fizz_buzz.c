eb13a71336c980d48f1851ead0701dca251eadbe
koryos77 <k.rouget21@gmail.com>
2024-10-28 20:08:51 +0100

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,16 +14,23 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		} else if (i % 3 == 0)
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
-		} else if (i % 5 == 0)
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
-		} else
+			printf("Buzz");
+		}
+		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
