d91e1fe17db095ecfc29de541cb31c58619eb59a
koryos77 <k.rouget21@gmail.com>
2024-10-30 19:59:37 +0100

    print aray

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 
 void puts_half(char *str)
 {
-	int i = 5;
+	int i = 0;
 
 	for (; str[i] != '\0'; str++)
 	{
---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - a pointer to print array
+ * @a: pointer
+ * @n: number of element to print
+ * Return: on Sucess 1
+ * On error, 0 is returned
+ */
+
+void print_array(int *a, int n)
+{
+	for (n = 0; n < 5; n++)
+	{
+		printf("%d", a[n]);
+
+		if (n < 4)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
