ef31e2269cf9ad63532ab6d1dc47c0cc8a922c51
koryos77 <k.rouget21@gmail.com>
2024-10-30 19:30:01 +0100

    rev string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,23 @@
 
 void rev_string(char *s)
 {
+	int length = 0;
+	int i;
+	char reversed[10];/*create an array for the reversed string*/
 
+	while (s[length] != '\0')/*find the length of the string*/
+	{
+		length++;
+	}
+
+	for (i = 0; i < length; i++)/*fill the array*/
+	{
+		reversed[i] = s[length - 1 - i];
+	}
+	reversed[length] = '\0';
+
+	for (i = 0; i <= length; i++)/*copy the reversed string in the array*/
+	{
+		s[i] = reversed[i];
+	}
 }
