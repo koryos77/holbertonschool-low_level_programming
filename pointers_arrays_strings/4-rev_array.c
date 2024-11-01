919b5bd216eac8e566f956a292c5f5ff74002d48
koryos77 <k.rouget21@gmail.com>
2024-11-01 17:58:11 +0100

    rev_array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -11,12 +11,14 @@ void reverse_array(int *a, int n)
 {
 	int *start = a;
 	int *end = a + n - 1;
-	int temp = *start;
+	int temp;
 
 	while (start < end)
 	{
+	temp = *start;
 	*start = *end;
 	*end = temp;
+
 	start++;
 	end--;
 	}
