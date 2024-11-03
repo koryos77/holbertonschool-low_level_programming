f486a83ebb78537424acb9e94f7b25b46edc1253
koryos77 <k.rouget21@gmail.com>
2024-11-03 12:04:02 +0100

    finally

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,5 +8,25 @@
 
 int _atoi(char *s)
 {
+	int i = 0;
+	int sign = 1;
+	unsigned int result = 0;
 
+	while (s[i] == ' ')
+		i++;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (result > 0)
+		break;
+		i++;
+	}
+
+	return (result * sign);
 }
