3a1239d2e6978417f189096fc5a249c6e9bc2039
koryos77 <k.rouget21@gmail.com>
2024-11-07 17:00:19 +0100

    newname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This function is main
+ * @argc: arguments contents
+ * @argv: arguments vectors
+ * Return: on Sucess 1
+ * On error, 0 is returned
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
