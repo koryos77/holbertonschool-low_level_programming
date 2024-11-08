2ab7282b06be4cdb517e5bf426ecc5423c450573
koryos77 <k.rouget21@gmail.com>
2024-11-08 13:59:06 +0100

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This function is main
+ * @argc: arguments contents
+ * @argv: arguments vectors
+ * Return: on Sucess 1
+ * On error, 0 is returned
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
