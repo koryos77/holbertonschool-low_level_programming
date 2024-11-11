02247d1a6712231994cdcf07f159c53177b80631
koryos77 <k.rouget21@gmail.com>
2024-11-11 14:11:28 +0100

    Duplicated

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,5 +9,25 @@
 
 char *_strdup(char *str)
 {
+	int i = 0;
+	int j = 0;
+	char *ptr;
 
+	if (str == NULL)
+	return (NULL);
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * (i + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+	{
+		ptr[j] = str[j];
+	}
+	return (ptr);
 }
