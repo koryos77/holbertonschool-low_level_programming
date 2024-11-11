1854885a6834677020868426d065907f82d4e3d3
koryos77 <k.rouget21@gmail.com>
2024-11-11 13:36:51 +0100

    create array

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - function that duplicates a string
+ * @str: string to duplicate
+ * Return: NULL if str = NULL or insufficient memory
+ * On success return *str
+ */
+
+char *_strdup(char *str)
+{
+
+}
