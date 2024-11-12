f1777c480bf0ecc8c51c9cd0827a69acdad1ca4d
koryos77 <k.rouget21@gmail.com>
2024-11-12 15:38:54 +0100

    coucou Jules

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: array of nmemb element
+ * @size: bytes
+ * Return: NULL on failure
+ * If NULL is passed, treat it as an empty string
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
