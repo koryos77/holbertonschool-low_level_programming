efa440cc25752af702ed88246185bddcc158e3e2
koryos77 <k.rouget21@gmail.com>
2024-11-12 15:28:33 +0100

    nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= j)
 	n = j;
 
-	str = malloc(sizeof(char) * (i + j + 1));
+	str = malloc(sizeof(char) * (i + n + 1));
 
 	if (str == NULL)
 	return (NULL);
