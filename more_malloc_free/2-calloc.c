d1daf00b0941207f93437e5e9635caf024f748c1
koryos77 <k.rouget21@gmail.com>
2024-11-12 16:40:46 +0100

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,5 +10,27 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+	unsigned char *str;
+	unsigned int total = 0;
+	unsigned int i = 0;
 
+	if ((nmemb == 0) || (size == 0))
+	return (NULL);
+
+	total = nmemb * size;
+
+	ptr = malloc(total);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	str = (unsigned char *)ptr;
+	for (i = 0; i < total; i++)
+	{
+		str[i] = 0;
+	}
+	return (ptr);
 }
