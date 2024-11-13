eb0d94865891b02f6a3f5a8f9f3f6c9059d69100
koryos77 <k.rouget21@gmail.com>
2024-11-13 14:45:31 +0100

    typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: Name of the dog owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
