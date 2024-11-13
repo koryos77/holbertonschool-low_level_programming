db1710f5a3c16e14358ad1f2401567ecce1f3a38
koryos77 <k.rouget21@gmail.com>
2024-11-13 16:12:57 +0100

    the new poppy

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - function that print poppy's infos
+ * @name: pointer to the name
+ * @age: age of the new dog
+ * @owner: owner of the new dog
+ * store a copy of name and owner
+ * Return: NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0;
+	int j = 0;
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
+	return (NULL);
+
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		d->name = malloc(i + 1);
+		if (d->name == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+	}
+	for (j = 0; j < i; j++)
+	{
+		d->name[j] = name[j];
+	}
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		d->owner = malloc(i + 1);
+		if (d->owner == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+	}
+	for (j = 0; j < i; j++)
+	{
+		d->owner[j] = owner[j];
+	}
+	d->age = age;
+	return (d);
+}
