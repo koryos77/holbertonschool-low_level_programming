f6d4cc9e5e79cad928ab704ae967955379bbd286
koryos77 <k.rouget21@gmail.com>
2024-11-13 13:39:44 +0100

    doggo poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is the structure for dog infos
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
