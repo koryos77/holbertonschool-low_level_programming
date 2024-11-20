d1511d6cfe6192574f3396c1c45296261f933687
koryos77 <k.rouget21@gmail.com>
2024-11-20 11:39:14 +0100

    checker

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 
@@ -16,14 +15,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(args, int);
-	}
-
 	if (n == 0)
 	{
 	return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
 	}
 
 	va_end(args);
