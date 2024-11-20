5da33bfcfd921fcbcdf1a5b33b2d33fdc70d89c3
koryos77 <k.rouget21@gmail.com>
2024-11-20 09:26:57 +0100

    &&

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - check the code
+ * @n: int n
+ * Return: 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
