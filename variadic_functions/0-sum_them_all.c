4e431864b961f922610de4b8ff43d0a21da3c2f7
koryos77 <k.rouget21@gmail.com>
2024-11-20 11:37:01 +0100

    checker

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -16,15 +16,16 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
 	if (n == 0)
 	{
 	return (0);
 	}
 
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(args, int);
-	}
 	va_end(args);
 	return (sum);
 }
