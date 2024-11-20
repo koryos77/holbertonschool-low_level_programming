3f126b5cfb6993c218409a37d5bd69bc52382ccd
koryos77 <k.rouget21@gmail.com>
2024-11-20 13:45:41 +0100

    update file

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - check the code
+ * @n: int n
+ * Return: 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	int sum = 0;
+
+	va_start(args, n);
+
+	if (n == 0)
+	{
+	return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+	return (sum);
+}
