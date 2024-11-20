84d9941cf0158f3f19f469c0f07cf31e7eae66aa
koryos77 <k.rouget21@gmail.com>
2024-11-20 15:44:34 +0100

    prep

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints strings
+ * @format: is a list of types of arguments passed to the function
+ * Return: Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+
+}
